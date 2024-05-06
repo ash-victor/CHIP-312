19a9842a7bef7640e6747c2859f0f47e6e475680
Ashley Victor <ashv@unc.edu>
2024-05-06 14:11:44 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+
+import './App.css';
+import MapComponent from './MapComponent';
+
+function App() {
+  return (
+    <div className="App">
+       <Header />
+      <MapComponent />
+    </div>
+  );
+}
+
+export default App;
